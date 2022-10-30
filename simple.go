faf2927f548d33f3f78a80b298afe969bf4ffde3
Pranaav S <85563271+Pranaav-S@users.noreply.github.com>
2022-10-30 13:15:21 +0530

    Added comments

--- simple.go
+++ simple.go
@@ -1,6 +1,6 @@
 package main
-import "fmt"
-func main(){
-  fmt.Println("hello world")
+import "fmt"  //package fmt improted to use Println method
+func main(){    
+  fmt.Println("hello world") //Prints hello world in the output screen
 
 }
