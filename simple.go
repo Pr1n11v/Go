0f4ceef39710ea48f7267466811bc666b05dba26
Pranaav-S <pranaavsathish1974@gmail.com>
2022-10-31 18:36:01 +0530

    The comments were rectified

--- simple.go
+++ simple.go
@@ -1,5 +1,5 @@
 package main
-import "fmt"  //package fmt improted to use Println method
+import "fmt"  //package fmt imported to use Println method
 func main(){    
   fmt.Println("hello world") //Prints hello world in the output screen
 
