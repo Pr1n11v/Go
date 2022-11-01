749ff3e9eb37398ec9a5569cd55f00effe203d13
Pranaav-S <pranaavsathish1974@gmail.com>
2022-11-01 16:46:30 +0530

    comments were added to palindromeNumber.go

--- leetCode/palindromeNumber.go
+++ leetCode/palindromeNumber.go
@@ -1,16 +1,16 @@
 package main
 import (
-    "fmt"
-    "strconv"
+    "fmt"  
+    "strconv" //strconv for to and from string conversions
     )
 
 func isPalindrome(x int) bool {
-    str:=strconv.Itoa(x)
-       var revStr string
-       for i:=len(str)-1;i>=0;i--{
+    str:=strconv.Itoa(x) //converts integer x to string
+       var revStr string  
+       for i:=len(str)-1;i>=0;i--{  // for loop for iterating through the string in reverse 
            revStr+=string(str[i])
        }
-    if str == revStr{
+    if str == revStr{ //Checks if the string  and reverse of the string is equal
         return true 
     }
     return false
@@ -18,8 +18,8 @@ func isPalindrome(x int) bool {
 
 
 func main(){
-  var num int  //creating a integer variable
+  var num int  //creating an integer variable
   fmt.Println("Enter a number greater than 9")
-  fmt.Scan(&num)
-  fmt.Println(isPalindrome(num))
+  fmt.Scan(&num) //accepting the number from user
+  fmt.Println(isPalindrome(num)) //calling the ispalindrome function
 }
