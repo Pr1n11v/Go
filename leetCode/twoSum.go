7f9268b348fec837cc726486201bdfa1d245e6b0
Pranaav-S <pranaavsathish1974@gmail.com>
2022-11-15 19:47:08 +0530

    Program to find a slice of two numbers that add upto target

---
+++ leetCode/twoSum.go
@@ -0,0 +1,33 @@
+package main
+import "fmt"
+
+func twoSum(nums []int, target int) []int {
+    for i,data:=range nums{
+      for j,data2:=range nums{
+        if i==j{
+            continue
+        }else{
+        if (data+data2)==target{
+            return []int{i,j} 
+        }
+        }
+    }  
+    }
+    return []int{0}
+}
+
+func main(){
+  var size,target int 
+  fmt.Println("Enter the size of the array and the target sum")
+  fmt.Scan(&size,&target)
+  var arr := make([]int,size)
+  for i:=0;i<size;i++{
+       fmt.Scanf("%d",&arr[i])
+   
+  } 
+  
+   arr2:=twoSum(arr,target)
+   fmt.println(arr2)
+ 
+}
+
